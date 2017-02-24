d1b92432755e56aec072a8e5de47e469c8b08917
Scott Harrison <scottrules44@gmail.com>
2017-02-23 22:10:56 -0600

    1.1
    1.1

--- main.lua
+++ main.lua
@@ -2,4 +2,3 @@ local firebase = require "plugin.firebaseAnalytics"
 
 firebase.init()
 firebase.LogEvent("select_content", {content_type = "hello", item_id= "world"})
-firebase.SetUserProperties("favorite_food", "burger")
